7d2949e632ace88a7d76ad503c2f597602fe8a99
rio-laptop <yakimenko.kir@gmail.com>
2024-06-13 11:15:29 +0200

    Present

--- SCOL/src/main/scala/Main.scala
+++ SCOL/src/main/scala/Main.scala
@@ -1,5 +1,3 @@
-package main.scala
-
 import main.scala.Lexer.lex
 import main.scala.Names.{Infix, NonAssoc, RightAssoc, isNumeric, setFixity, setTypeFixity}
 import main.scala.Parser.parseType
@@ -8,23 +6,29 @@ import main.scala.Type.{HolType, mkCompType, primNewTyconst}
 import main.scala.TypeAnal.*
 
 
-@main
-def mAin(): Unit = {
-    
-    
-    println(lex("a".toList))
-    println(lex("1 12".toList))
-    println(lex("(12)\\".toList))
-    println(lex("\\(\"23\")\\".toList))
+object Main extends App{
 
-    setTypeFixity("->", Infix(5, RightAssoc))
-    primNewTyconst("->", 2)
+    run()
 
-    primNewTyconst("bool", 0)
-    val boolTy: HolType = mkCompType("bool", List.empty)
-    setFixity("=", Infix(30, NonAssoc))
-    primNewConst("=", parseType("'a->'a->bool"))
+    private def run(): Unit = {
 
-    println(lex("12=11".toList))
 
+        println(lex("a".toList))
+        println(lex("1 12".toList))
+        println(lex("(12)\\".toList))
+        println(lex("\\(\"23\")\\".toList))
+
+
+        setTypeFixity("->", Infix(5, RightAssoc))
+        primNewTyconst("->", 2)
+
+        primNewTyconst("bool", 0)
+        val boolTy: HolType = mkCompType("bool", List.empty)
+        setFixity("=", Infix(30, NonAssoc))
+        primNewConst("=", parseType("'a->'a->bool"))
+
+        println(lex("12=11".toList))
+
+        println(parseType("'a->'a->bool"))
+    }
 }
